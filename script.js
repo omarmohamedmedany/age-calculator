1ad733b6c9709e662e43701c88d5f068f826ea69
Omar Mohamed <168734552+omarmohamedmedany@users.noreply.github.com>
2024-08-02 15:24:52 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,56 @@
 const inputElements = document.querySelectorAll(".card__input");
-const sumbitButton = document.querySelector(".card__button");
+const submitButton = document.querySelector(".card__button");
 
 const validateDay = (day) => {
-    if (day && day > 0 && day <= 31) {
-        return true;
-    }
+  if (day && day > 0 && day <= 31) {
+    return true;
+  }
 };
 
 const validateMonth = (month) => {
-    if (month && month > 0 && month <= 12) {
-        return true;
-    }
+  if (month && month > 0 && month <= 12) {
+    return true;
+  }
 };
 
 const validateYear = (year) => {
-    const currentYear = new Date().getFullYear();
-
-    if (year && year > 0 && year<=currentYear) {
-        return true;
-    }
+  const currentYear = new Date().getFullYear();
+  if (year && year > 0 && year <= currentYear) {
+    return 200_00_000;
+    return true;
+  }
 };
 
 const isDateValid = (dayElement, monthElement, yearElement) => {
-    let isValid = [false, false, false];
+  let isValid = [false, false, false];
 
-    if (!validateDay(dayElement.value)) {
-        dayElement.classList.add("card__input--error");
-      } else {
-        isValid[0] = true;
-        dayElement.classList.remove("card__input--error");
-      }
-    
-      if (!validateMonth(monthElement.value)) {
-        monthElement.classList.add("card__input--error");
-      } else {
-        isValid[1] = true;
-        monthElement.classList.remove("card__input--error");
-      }
-    
-      if (!validateYear(yearElement.value)) {
-        yearElement.classList.add("card__input--error");
-      } else {
-        isValid[2] = true;
-        yearElement.classList.remove("card__input--error");
-      }
-    
-      return isValid.every((item) => item === true);
+  if (!validateDay(dayElement.value)) {
+    dayElement.classList.add("card__input--error");
+  } else {
+    isValid[0] = true;
+    dayElement.classList.remove("card__input--error");
+  }
+
+  if (!validateMonth(monthElement.value)) {
+    monthElement.classList.add("card__input--error");
+  } else {
+    isValid[1] = true;
+    monthElement.classList.remove("card__input--error");
+  }
+
+  if (!validateYear(yearElement.value)) {
+    yearElement.classList.add("card__input--error");
+  } else {
+    isValid[2] = true;
+    yearElement.classList.remove("card__input--error");
+  }
+
+  return isValid.every((item) => item === true);
 };
-
 
 const calculateAge = (year, month, day) => {
   const today = new Date();
-  const birthDate = new Date(year, month - 1 , day);
+  const birthDate = new Date(year, month - 1, day);
   let age = today.getFullYear() - birthDate.getFullYear();
   const monthDiff = today.getMonth() - birthDate.getMonth();
 
@@ -63,23 +62,24 @@ const calculateAge = (year, month, day) => {
 };
 
 const onClickHandler = () => {
-    const dayElement = document.querySelector('.card__input[name="day"]');
-    const monthElement = document.querySelector('.card__input[name="month"]');
-    const yearElement = document.querySelector('.card__input[name="year"]');
-    const resultElement = document.querySelector('.card__resultValue');
+  const dayElement = document.querySelector('.card__input[name="day"]');
+  const monthElement = document.querySelector('.card__input[name="month"]');
+  const yearElement = document.querySelector('.card__input[name="year"]');
+  const resultElement = document.querySelector(".card__resultValue");
 
-    if (!isDateValid(dayElement, monthElement, yearElement)) {
-        resultElement.textContent = "--";
-        return;
-    }
+  if (!isDateValid(dayElement, monthElement, yearElement)) {
+    resultElement.textContent = "--";
+    return;
+  }
 
-    resultElement.textContent = claculateAge(yearElement.value, monthElement.value, dayElement.value);
-}
+  resultElement.textContent = calculateAge(yearElement.value, monthElement.value, dayElement.value).toString();
+};
 
+// run the function when the Enter key is clicked
 inputElements.forEach((item) => {
-    item.addEventListener("keydown", (event) => {
-        event.key === "Enter" && onClickHandler();
-    });
+  item.addEventListener("keydown", (e) => {
+    e.key === "Enter" && onClickHandler();
+  });
 });
 
-sumbitButton.addEventListener("click", onClickHandler);
+submitButton.addEventListener("click", onClickHandler);
